7747a5568bc8ac2b27847660bde586e7f9a5e8ed
amckasson90@gmail.com <amckasson90@gmail.com>
2021-06-08 13:39:03 -0400

    Initial Commit

---
+++ BFGCoffeeShop.WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace BFGCoffeeShop.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ BFGCoffeeShop.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BFGCoffeeShop.WebAPI.WebApiApplication" Language="C#" %>
